c8aacfc37a3841f11ce1863cfa3edbc178e0371d
Krzysztof Sokalla <k.sokalla@gmail.com>
2018-05-13 01:45:09 +0200

    search bar

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function () {
+	'use strict';
+	jQuery("#woocommerce-product-search-field-0").attr("placeholder", "Szukaj...");
+	jQuery('.woocommerce-product-search').append(jQuery('<img>',{id:'lupka',src:'./wp-content/themes/storefront-child/img/lupka.png'}));
+	jQuery('#lupka').width(34);
+	jQuery('#lupka').height(34);
+});				
